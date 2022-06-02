6b49cdd1bdfd4cc491f862c3b0fd3f2b2b7faa84
aokiji123 <jenkaboy7@gmail.com>
2022-06-02 11:57:54 +0200

    homework finished

---
+++ 11-homework.cpp
@@ -0,0 +1,30 @@
+// HackerRank
+#include <iostream>
+using namespace std;
+
+int main() {
+  int number;
+  cin >> number;
+  
+  if (number == 1) {
+    cout << "one";
+  } else if (number == 2) {
+    cout << "two";
+  } else if (number == 3) {
+    cout << "three";
+  } else if (number == 4) {
+    cout << "four";
+  } else if (number == 5) {
+    cout << "five";
+  } else if (number == 6) {
+    cout << "six";
+  } else if (number == 7) {
+    cout << "seven";
+  } else if (number == 8) {
+    cout << "eight";
+  } else if (number == 9) {
+    cout << "nine";
+  } else {
+    cout << "Greater than 9";
+  }
+}

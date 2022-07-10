a2da2ea90367921143b73f0c9dba40243e661743
aokiji123 <jenkaboy7@gmail.com>
2022-07-10 21:01:53 +0200

    homework finished

---
+++ 21-homework.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int random(int min, int max)
+{
+  int k = max - min + 1;
+  return min + rand() % k;
+}
+
+int main()
+{
+  srand(time(0));
+  rand();
+
+  int counter = 0;
+  int M, N;
+  int k;
+
+  cin >> M;
+  cin >> N;
+  cin >> k;
+
+  int array[M][N];
+
+  for (int i = 0; i < M; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
+      array[i][j] = random(0, 1);
+    }
+  }
+
+  cout << "\n";
+  for (int i = 0; i < M; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
+      cout << array[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+  cout << "\n";
+  for (int i = 0; i < M; i++)
+  {
+
+    for (int j = 0; j < N; j++)
+    {
+
+      if (array[i][j] == 0)
+      {
+        counter++;
+
+        if (counter == k)
+        {
+          cout << "Место свободно"
+               << "\n";
+        }
+      }
+      else
+      {
+        counter = 0;
+        cout << "Место занято"
+             << "\n";
+      }
+    }
+    counter = 0;
+    cout << "\n";
+  }
+}
